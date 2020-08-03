328da122fa53121a0728307c202178abe99e5702
Sam Feng <yfeng68@ucsc.edu>
2020-08-02 18:29:21 -0600

    Box added
    Added in droppable box

--- Objects/Sushi_Piece.gd
+++ Objects/Sushi_Piece.gd
@@ -3,11 +3,28 @@ extends "res://Scripts/DraggableArea2D.gd"
 
 export (Global.INGREDIENT) var sushi_type = Global.INGREDIENT.SALMON
 
+var droppable = false
 
-func _ready():
-	pass # Replace with function body.
+# Reference for the dropped box
+var box_ref
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Drop and kill the ingredient 
+func _on_Sushi_Piece_on_released():
+	if droppable == true:
+		box_ref.dropped()
+		queue_free()
+
+
+# Check if the box is the right box
+# Set droppable to true if it is the right box
+func _on_Sushi_Piece_area_entered(area):
+	if "Box" in area.name:
+		box_ref = area
+		
+		if area.ingredient == sushi_type:
+			droppable = true
+
+
+func _on_Sushi_Piece_area_exited(area):
+	droppable = false
