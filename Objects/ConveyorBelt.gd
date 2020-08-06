71df46ca1cc412ae101dbdce8e730075323ec277
Sam Feng <yfeng68@ucsc.edu>
2020-08-06 00:27:37 -0600

    Main Menu Transition

--- Objects/ConveyorBelt.gd
+++ Objects/ConveyorBelt.gd
@@ -8,3 +8,9 @@ func _on_Timer_timeout():
 	var p = sushi_plate.instance()
 	p.set_position($SpawnPosition.get_position())
 	add_child(p)
+
+
+# Change speed at runtime
+# Normally it's -0.156
+func change_conveyor_speed(speed: float):
+	$Belt.get_material().set_shader_param("speed", speed)
---
+++ Objects/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+func _ready():
+	get_tree().set_pause(true)
+
+
+func _on_Play_pressed():
+	$AnimationPlayer.play("intro")
+	print("Play!")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if "intro" in anim_name:
+		get_tree().set_pause(false)
