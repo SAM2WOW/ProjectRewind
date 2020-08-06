060f420edfd2d87421d2c9d93370ac4f2789ddfb
Sam Feng <yfeng68@ucsc.edu>
2020-08-06 00:01:24 -0600

    Update Conveyor Belt Test

--- Objects/ConveyorBelt.gd
+++ Objects/ConveyorBelt.gd
@@ -8,26 +8,7 @@ const WIDTH = 1912
 var sushi_plate = preload("res://Objects/Sushi/Sushi_Plate.tscn")
 
 
-func _process(delta):
-	"""
-	# Endless scrolling
-	$Belt.translate(Vector2(SPEED * delta, 0))
-	$Belt2.translate(Vector2(SPEED * delta, 0))
-	
-	if $Belt.position.x <= -WIDTH:
-		$Belt.position.x = WIDTH
-	
-	if $Belt2.position.x <= -WIDTH:
-		$Belt2.position.x = WIDTH
-	"""
-
-
 func _on_Timer_timeout():
 	var p = sushi_plate.instance()
 	p.set_position($SpawnPosition.get_position())
 	add_child(p)
-
-
-func _on_HSlider_value_changed(value):
-	$Belt.get_material().set_shader_param("speed",value)
-	$Label.set_text(str(value))
