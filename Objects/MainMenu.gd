3397f0bb362754267998ac23f1306e99db24ff87
Sam Feng <yfeng68@ucsc.edu>
2020-08-06 23:16:17 -0600

    Audio Muting Added

--- Objects/MainMenu.gd
+++ Objects/MainMenu.gd
@@ -16,3 +16,13 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 	if "scene" in anim_name:
 		get_tree().set_pause(false)
 		
+
+
+# Settings
+
+func _on_Music_toggled(button_pressed):
+	AudioServer.set_bus_mute(2, not button_pressed)
+
+
+func _on_Sounds_toggled(button_pressed):
+	AudioServer.set_bus_mute(1, not button_pressed)
