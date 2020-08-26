a2ce574ff23f12788863083917b0c3690cbd7f44
Sam Feng <yfeng68@ucsc.edu>
2020-08-26 16:20:59 -0600

    Fix the button icon bug

--- Objects/Tools/misc/ButtonCoolDown.gd
+++ Objects/Tools/misc/ButtonCoolDown.gd
@@ -12,3 +12,23 @@ func _on_TextureButton_pressed():
 
 func _on_Timer_timeout():
 	pass
+
+
+# Sync state of button with progress bar
+# NEED: optimization so it doesn't run everyframe
+func _process(_delta):
+	if is_pressed() or is_disabled():
+		$"../ProgressBarDown".show()
+		$"../ProgressBarUp".hide()
+		
+		# Change button icon visibility
+		$"../Number".set_position(Vector2(-277, -17))
+		$"../Icon".set_position(Vector2(-330, -9))
+	else:
+		$"../ProgressBarDown".hide()
+		$"../ProgressBarUp".show()
+	
+		# Reset Icon Position
+		$"../Number".set_position(Vector2(-277, -56))
+		$"../Icon".set_position(Vector2(-330, -48))
+
