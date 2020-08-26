90532705702182874b94e7237807d5200d94f350
Sam Feng <yfeng68@ucsc.edu>
2020-08-26 16:21:11 -0600

    Add touch for draggable 2d

--- Scripts/DraggableArea2D.gd
+++ Scripts/DraggableArea2D.gd
@@ -19,15 +19,19 @@ func _ready():
 	original_position = get_position()
 
 
-func _input(_event):
+func _input(event):
+	# Check for touch
+	var touch = event is InputEventScreenTouch
+	var pressed_state = event.is_pressed()
+	
 	# Turn on dragging when mouse clicked on the area
-	if is_mouse_hover and Input.is_action_just_pressed("touch") and is_draggable:
+	if is_mouse_hover and (Input.is_action_just_pressed("touch") or (touch and pressed_state)) and is_draggable:
 		is_dragging = true
 		
 		Global.mouse_selected = true
 		emit_signal("on_dragging")
 		
-	if is_dragging and Input.is_action_just_released("touch"):
+	if is_dragging and (Input.is_action_just_released("touch") or (touch and not pressed_state)):
 		is_dragging = false
 		
 		Global.mouse_selected = false
