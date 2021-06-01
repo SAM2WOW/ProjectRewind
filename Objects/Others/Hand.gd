7d820f8a23256915bea1a93b19065a83da0aadb8
Sam Feng <fengyuan20000303@gmail.com>
2021-06-01 13:54:09 -0700

    Android Touch Update 2021/6/1

--- Objects/Others/Hand.gd
+++ Objects/Others/Hand.gd
@@ -5,11 +5,18 @@ const hand_colors = ["ffffff", "efd0a3", "a58557"]
 var hand_open = preload("res://Arts/open_hand.png")
 var hand_close = preload("res://Arts/closed_hand.png")
 
+var touch_screen = false
+
 
 func _ready():
 	# Check if device is touch supported
 	if OS.has_touchscreen_ui_hint():
-		queue_free()
+		touch_screen = true
+		
+		$Sprite.hide()
+		$Sprite.set_texture(hand_close)
+		$Sprite.set_modulate(Color("bdffffff"))
+		
 	else:
 		# Hide mouse while in game
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -24,7 +31,11 @@ func _process(_delta):
 	set_global_position(get_global_mouse_position())
 
 
-func _input(_event):
+func _input(event):
+	if touch_screen:
+		if event is InputEventScreenTouch:
+			$Sprite.set_visible(event.pressed)
+	
 	# Change sprites
 	if Input.is_action_just_pressed("touch"):
 		
